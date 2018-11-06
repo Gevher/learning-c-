6bb185c828975d8d897fcf77e3188ad62a130904
Gevher <tomasz.omasta@gmail.com>
2018-11-06 11:33:55 +0100

    Prepared window

--- WHerrCheck/main.cpp
+++ WHerrCheck/main.cpp
@@ -1,39 +1,40 @@
 #if defined(UNICODE) && !defined(_UNICODE)
-    #define _UNICODE
+#define _UNICODE
 #elif defined(_UNICODE) && !defined(UNICODE)
-    #define UNICODE
+#define UNICODE
 #endif
 
 #include <tchar.h>
 #include <windows.h>
-
+#include <cstdlib>
+#include <iostream>
 
 LPSTR NazwaKlasy = "Klasa Okienka";
 MSG Komunikat;
+HWND hWindow;
 HWND g_hButton;
 HWND g_hButton2;
+HWND g_hExitButton;
 HWND hTextbox1;
+//handler okna konsoli
+HWND cmdhwnd;
 
 LRESULT CALLBACK WndProc( HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam );
+void HideConsole();
+using namespace std;
 
 int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow )
 {
 
 
     WNDCLASSEX window1;
-    HWND hWindow;
-
-
     window1.cbSize = sizeof(WNDCLASSEX);
     window1.style = 0;
     window1.lpfnWndProc = &WndProc;
-    window1.cbClsExtra = NULL;
-    window1.cbWndExtra = NULL;
     window1.hCursor = LoadCursor(NULL, IDI_APPLICATION);
     window1.hIcon = LoadIcon (NULL, IDI_APPLICATION);
     window1.hInstance = hInstance;
     window1.hbrBackground = ( HBRUSH )( COLOR_WINDOW + 1);
-    window1.lpszMenuName = NULL;
     window1.lpszClassName = NazwaKlasy;
     window1.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
 
@@ -45,11 +46,16 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLin
 
 
     //Tworzenie okna
-    hWindow = CreateWindowEx(WS_EX_WINDOWEDGE, NazwaKlasy, "Œwinie", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 400, 500, NULL, NULL, hInstance, NULL);
-    g_hButton = CreateWindowEx (0, "BUTTON", "Dupa", WS_CHILD|WS_VISIBLE, 20, 20, 90, 30, hWindow, NULL, hInstance, NULL);
-    g_hButton2 = CreateWindowEx (0, "BUTTON", "Du¿a Dupa", WS_CHILD|WS_VISIBLE, 20, 60, 90, 30, hWindow, NULL, hInstance, NULL);
-    hTextbox1 = CreateWindowEx( WS_EX_CLIENTEDGE, "EDIT", NULL, WS_CHILD | WS_VISIBLE | WS_BORDER |
-            WS_VSCROLL | ES_MULTILINE | ES_AUTOVSCROLL, 20, 90, 150, 151, hWindow, NULL, hInstance, NULL);
+    hWindow = CreateWindowEx(WS_EX_WINDOWEDGE, NazwaKlasy, "Error Checker", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 325, 375, NULL, NULL, hInstance, NULL);
+    g_hButton = CreateWindowEx (0, "BUTTON", "Start", WS_CHILD|WS_VISIBLE, 20, 20, 90, 30, hWindow, NULL, hInstance, NULL);
+    g_hButton2 = CreateWindowEx (0, "BUTTON", "Kopiuj liste", WS_CHILD|WS_VISIBLE, 20, 60, 90, 30, hWindow, NULL, hInstance, NULL);
+    g_hExitButton = CreateWindowEx(0, "BUTTON", "Zakoncz", WS_CHILD|WS_VISIBLE, 20, 100, 90, 30, hWindow, NULL, hInstance, NULL);
+    hTextbox1 = CreateWindowEx( WS_EX_CLIENTEDGE, "EDIT", NULL, WS_CHILD | WS_VISIBLE | WS_BORDER |ES_READONLY|
+            WS_VSCROLL | ES_MULTILINE | ES_AUTOVSCROLL, 130, 20, 150, 300, hWindow, NULL, hInstance, NULL);
+
+
+
+
 
     if (hWindow == NULL || g_hButton == NULL)
     {
@@ -58,17 +64,24 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLin
     }
     ShowWindow(hWindow, nCmdShow);
     UpdateWindow(hWindow);
-
+    HideConsole();
     while( GetMessage(&Komunikat, NULL, 0, 0))
     {
         TranslateMessage(&Komunikat);
         DispatchMessage(&Komunikat);
     }
+
     return Komunikat.wParam;
+}
 
 
-
-    return 0;
+void HideConsole()
+{
+    char consoleName[500];
+    GetConsoleTitleA(consoleName, 500);
+    cmdhwnd = FindWindowA(NULL, consoleName);
+    ShowWindow(cmdhwnd, SW_HIDE);
+    FreeConsole();
 }
 
 LRESULT CALLBACK WndProc( HWND hWindow, UINT msg, WPARAM wParam, LPARAM lParam)
@@ -78,7 +91,6 @@ LRESULT CALLBACK WndProc( HWND hWindow, UINT msg, WPARAM wParam, LPARAM lParam)
             case WM_CLOSE:
             DestroyWindow(hWindow);
             break;
-
             case WM_DESTROY:
             PostQuitMessage(0);
             break;
@@ -93,6 +105,6 @@ LRESULT CALLBACK WndProc( HWND hWindow, UINT msg, WPARAM wParam, LPARAM lParam)
             default:
             return DefWindowProc( hWindow, msg, wParam, lParam);
         }
-
+        FreeConsole();
         return 0;
     }
