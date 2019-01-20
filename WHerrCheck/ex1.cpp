6d4cb9fce2317467ae32490fc3c41f7465383893
Gevher <tomasz.omasta@gmail.com>
2019-01-20 18:49:11 +0100

    Update ex1.cpp

--- WHerrCheck/ex1.cpp
+++ WHerrCheck/ex1.cpp
@@ -4,24 +4,65 @@
 
 using namespace std;
 
-int* createArray(int);
+
 void swapTwoValues (int*, int*);
+
 void printArray (int[], int);
-void bubbleSort (int[], int, int);
+int* createArray(int);
 void randomizeArray(int[], int);
+
+void bubbleSort (int[], int, int);
+void selectionSort(int[], int, int);
+
 
 int main ()
 {
-    int sizeOfArray;
+    int sizeOfArray, sortDirection;
+    string sortSelection;
 
     cout << "gib size" << endl;
     cin >> sizeOfArray;
 
     int* randomArray = createArray(sizeOfArray);
-
     randomizeArray(randomArray, sizeOfArray);
-    bubbleSort(randomArray, sizeOfArray, 1);
     printArray(randomArray, sizeOfArray);
+
+    cout << "gib sorting direction : \n(1 - incremental, 2 - decremental)" << endl;
+    cin>> sortDirection;
+    cout << "gib sorting algorithm type : \n(S - selection sort, B - bubble sort)" << endl;
+    cin >> sortSelection;
+
+    switch (sortSelection[0])
+    {
+    case 's':
+        {
+            selectionSort(randomArray, sizeOfArray, sortDirection);
+            break;
+        }
+    case 'S':
+        {
+            selectionSort(randomArray, sizeOfArray, sortDirection);
+            break;
+        }
+    case 'b':
+        {
+           bubbleSort(randomArray, sizeOfArray, sortDirection);
+            break;
+        }
+    case 'B':
+        {
+            bubbleSort(randomArray, sizeOfArray, sortDirection);
+            break;
+        }
+    default :
+        {
+            bubbleSort(randomArray, sizeOfArray,sortDirection);
+        }
+    }
+
+    printArray(randomArray, sizeOfArray);
+
+
     delete[] randomArray;
     return 0;
 }
@@ -41,41 +82,65 @@ void randomizeArray(int randomArray[], int sizeOfArray)
     srand(time(NULL));
 
 
-        for(int i = 0; i < sizeOfArray; i++)
+    for(int i = 0; i < sizeOfArray; i++)
     {
-        cout << "check[" << i << "]" << endl;
         randomArray[i] = rand();
-        cout << randomArray[i] << endl;
     }
 }
 void swapTwoValues (int * a, int* b)
 {
-    cout << *a << " > " << *b << endl;
+
     int c;
     c = *b;
     *b = *a;
     *a = c;
-    cout << *a << " < " << *b << endl;
 }
 
 void printArray(int randomArray[], int sizeOfArray)
 {
-    cout << "now dis" << endl;
     for (int i = 0; i< sizeOfArray; i++)
         cout << "Array [" << i << "] = " << randomArray[i] << endl;
 }
 
 void bubbleSort (int randomArray[], int sizeOfArray, int sortDirection)
 {
-    for (int i = 0; i < sizeOfArray; i++)
-    {
+    int tempVar = sizeOfArray;
 
-        cout << "stone[" << i << "]" << endl;
-        if (randomArray[i]>randomArray[i+1] && sortDirection == 1 && i!=(sizeOfArray-1))
+    do
+    {
+        for (int i = 0; i < tempVar; i++)
         {
-            swapTwoValues(&randomArray[i], &randomArray[i+1]);
-            cout << randomArray[i] << " < " << randomArray[i+1] << endl;
+            if (randomArray[i]>randomArray[i+1] && sortDirection == 1 && i!=(sizeOfArray-1))
+            {
+                swapTwoValues(&randomArray[i], &randomArray[i+1]);
+            }
+            else if (randomArray[i]<randomArray[i+1] && sortDirection == 2 && i!=(sizeOfArray-1))
+            {
+                swapTwoValues(&randomArray[i], &randomArray[i+1]);
+            }
         }
-        cout << endl << endl;
+    tempVar--;
     }
+    while(tempVar != 0);
+
+}
+
+void selectionSort (int randomArray[], int sizeOfArray, int sortDirection)
+{
+    for(int i = 0; i < sizeOfArray; i++)
+    {
+        for (int j = 1; j < (sizeOfArray-i); j++)
+        {
+            if (randomArray[i]>randomArray[i+j] && sortDirection == 1 && i!=(sizeOfArray-1))
+            {
+                swapTwoValues(&randomArray[i], &randomArray[i+j]);
+            }
+            else if (randomArray[i]<randomArray[i+j] && sortDirection == 2 && i!=(sizeOfArray-1))
+            {
+                swapTwoValues(&randomArray[i], &randomArray[i+j]);
+            }
+
+        }
+    }
+
 }
