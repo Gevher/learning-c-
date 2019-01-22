a2816490c1ecb42e36fc0c08ac3669eaafd1caeb
Gevher <tomasz.omasta@gmail.com>
2019-01-22 21:19:38 +0100

    Update ex8.cpp

--- ex8.cpp
+++ ex8.cpp
@@ -2,17 +2,45 @@
 
 using namespace std;
 
-book fillInfo();
-
+//inprogress
 struct book
 {
-        string autor, title;
-        int yearOfPublication
+        string author;
+        string title;
+        int yearOfPublication;
+
+
 }book1, book2, book3;
 
-int main
+
+book fillInfo(book);
+void printBookInfo(book);
+
+int main()
 {
+
+    fillInfo(book1);
+    fillInfo(book2);
+    fillInfo(book3);
+    printBookInfo(book1);
+    printBookInfo(book2);
+    printBookInfo(book3);
+
+
     return 0;
 }
 
-fill
+book fillInfo(book emptyBook)
+{
+    cout << "You are filling informations about new book. Please type in author : " << endl;
+    cin >> emptyBook.author;
+    cout << "Well done. Please type in title :" << endl;
+    cin >> emptyBook.title;
+    cout << "Well done. Please type in year of publication : " << endl;
+    cin >> emptyBook.yearOfPublication;
+}
+
+void printBookInfo (book filledBook)
+{
+    cout << filledBook.author << " , " << filledBook.title << " , " << filledBook.author << endl;
+}
