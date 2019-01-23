f7162124496c35e58c78199cee0f2a773b07e1bf
Gevher <tomasz.omasta@gmail.com>
2019-01-23 20:40:37 +0100

    update to Libary project

---
+++ Libary/book.h
@@ -0,0 +1,18 @@
+#ifndef BOOK_H_INCLUDED
+#define BOOK_H_INCLUDED
+
+
+class Books
+{
+    private:
+        std::string author;
+        std::string title;
+        std::string yearOfRealse;
+    public:
+        setAuthor(std::string);
+        setTitle(std::string);
+        setYearOfRealse(string);
+};
+
+
+#endif // BOOK_H_INCLUDED
---
+++ Libary/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "book.h"
+
+using namespace std;
+
+int main()
+{
+    string a;
+    cout << "Hello world!" << endl;
+    return 0;
+}
